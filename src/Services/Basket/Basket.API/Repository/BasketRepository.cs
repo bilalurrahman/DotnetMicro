20b25d588bca2a3a3a2fbb53a4a2dbc82bd4c07d
Bilal Ur Rahman <bilal.ur.rahman@hotmail.com>
2023-05-20 22:56:22 +0300

    --BasketsAPI

---
+++ src/Services/Basket/Basket.API/Repository/BasketRepository.cs
@@ -0,0 +1,40 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repository
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public BasketRepository(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache ?? throw new ArgumentNullException(nameof(distributedCache));
+        }
+
+        public async Task DeleteBasket(string userName)
+        {
+            await _distributedCache.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _distributedCache.GetStringAsync(userName);
+            if (String.IsNullOrEmpty(basket))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _distributedCache.SetStringAsync(basket.Username, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.Username);
+        }
+    }
+}
--- src/Services/Catalogs/Catalogs.API/Data/CatalogContext.cs
+++ src/Services/Catalogs/Catalogs.API/Data/CatalogContext.cs
@@ -15,7 +15,7 @@ namespace Catalogs.API.Data
             var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
             Products = database.GetCollection<Products>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
-
+            CatalogContextSeed.SeedData(Products);
         }
 
         public IMongoCollection<Products> Products { get; }
